4c470b768fd2ecf106eda20584bb0665112506b3
Jon Black <black@jon.black>
2020-08-10 09:17:06 -0400

    updated Grid spec to render child elements

--- src/components/grid/__tests__/Grid.spec.js
+++ src/components/grid/__tests__/Grid.spec.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { rendererWithTheme } from 'testing/utils';
+import { render, screen, rendererWithTheme } from 'testing/utils';
 
 import '@testing-library/jest-dom/extend-expect';
 
 import { Col, Container, Row } from '../index';
 
 describe('Grid', () => {
-  it('should render a row correctly with all props', () => {
-    const tree = rendererWithTheme(<Row>row content</Row>).toJSON();
+  it('should render a container correctly with all props', () => {
+    const tree = rendererWithTheme(<Container fluid>container content</Container>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('should render a container correctly with all props', () => {
-    const tree = rendererWithTheme(<Container fluid>container content</Container>).toJSON();
+  it('should render a row correctly', () => {
+    const tree = rendererWithTheme(<Row>row content</Row>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
@@ -31,5 +31,19 @@ describe('Grid', () => {
   it('should render order styles correctly with prop', () => {
     const tree = rendererWithTheme(<Col order={{ md: 1, lg: 2 }}>column content</Col>).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('children elements are correctly rendering', () => {
+    render(
+      <Container>
+        <Row>
+          <Col>placeholder text</Col>
+        </Row>
+      </Container>,
+    );
+
+    const children = screen.getByText('placeholder text');
+
+    expect(children).toBeInTheDocument();
   });
 });
